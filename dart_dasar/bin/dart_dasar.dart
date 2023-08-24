029ff116ee832e6c0f6fbeca6f38819972d9a858
diway <diway.afniwar@hotmail.com>
2023-08-25 02:28:37 +0700

    update dart

--- dart_dasar/bin/dart_dasar.dart
+++ dart_dasar/bin/dart_dasar.dart
@@ -1,5 +1,3 @@
-import 'package:dart_dasar/dart_dasar.dart' as dart_dasar;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_dasar.calculate()}!');
+void main(List<String> args) {
+  print("Hello the cruel world");
 }
